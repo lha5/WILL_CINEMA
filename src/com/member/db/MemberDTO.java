66928c0908cafd0d5664eec61f4ff76c4e467a29
ITWILL <ITWILL@DESKTOP-BFHD141>
2019-10-04 12:40:53 +0900

    MemberDTO 내용 수정

--- src/com/member/db/MemberDTO.java
+++ src/com/member/db/MemberDTO.java
@@ -1,5 +1,98 @@
 package com.member.db;
 
 public class MemberDTO {
-
+	private String id;
+	private String pass;
+	private String name;
+	private String birthday;
+	private String mobile;
+	private String email;
+	private String addr;
+	private int point;
+	private int level;
+	private String receive;
+	private String preference;
+	private int book_num;
+	private int buy_num;
+	
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public int getPoint() {
+		return point;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	public int getLevel() {
+		return level;
+	}
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	public String getReceive() {
+		return receive;
+	}
+	public void setReceive(String receive) {
+		this.receive = receive;
+	}
+	public String getPreference() {
+		return preference;
+	}
+	public void setPreference(String preference) {
+		this.preference = preference;
+	}
+	public int getBook_num() {
+		return book_num;
+	}
+	public void setBook_num(int book_num) {
+		this.book_num = book_num;
+	}
+	public int getBuy_num() {
+		return buy_num;
+	}
+	public void setBuy_num(int buy_num) {
+		this.buy_num = buy_num;
+	}
 }
