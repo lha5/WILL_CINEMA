a740b5b480b317cf60523a0c82bb04f1d2883d60
ITWILL <ITWILL@DESKTOP-BFHD141>
2019-10-04 13:50:39 +0900

    memloginaction

---
+++ src/com/member/action/MemberLoginAction.java
@@ -0,0 +1,24 @@
+package com.member.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.action.Action;
+import com.action.ActionForward;
+
+public class MemberLoginAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		System.out.println("MemberLoginAction! 실행문 나옵니다.");
+		
+		//id,pass
+		
+		//String id = 
+		
+		
+		return null;
+	}
+
+}
