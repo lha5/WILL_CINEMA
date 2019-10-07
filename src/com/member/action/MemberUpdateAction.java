aa61b46686b6f404200c9f3ff60f1238589b5b4f
ITWILL <ITWILL@DESKTOP-BFHD141>
2019-10-07 17:02:59 +0900

    update 완!(추후 수정)

--- src/com/member/action/MemberUpdateAction.java
+++ src/com/member/action/MemberUpdateAction.java
@@ -1,5 +1,7 @@
 package com.member.action;
 
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -31,7 +33,17 @@ public class MemberUpdateAction implements Action {
 		
 		request.setCharacterEncoding("UTF-8");
 		MemberDTO mdto = new MemberDTO();
+		// int - 우편번호만 
 		
+		mdto.setAddr(request.getParameter("addr"));
+		mdto.setBirthday(request.getParameter("birthday"));
+		mdto.setDetailaddr(request.getParameter("detailaddr"));
+		mdto.setEmail(request.getParameter("email"));
+		mdto.setMobile(request.getParameter("mobile"));
+		mdto.setName(request.getParameter("name"));
+		mdto.setPreference(request.getParameter("preference"));
+		mdto.setReceive(request.getParameter("receive"));
+		mdto.setZipcode(Integer.parseInt(request.getParameter("zipcode")));
 		
 		///////////////////////////////////
 		mdto.setId(id);
@@ -39,11 +51,47 @@ public class MemberUpdateAction implements Action {
 		
 		MemberDAO mdao = new MemberDAOImpl();
 		int update =  mdao.updateMember(mdto);
-		
-		// 무엇을 수정할것인가에대해서 이야기한후 작성 
-		
+		response.setContentType("text/html; charset=UTF-8");
 		
 		
+		if(update==-1){
+			PrintWriter out = response.getWriter();
+			
+			out.println("<script>");
+			out.println("  alert('아이디 없음!');  ");
+			out.println("  history.back(); ");
+			out.println("</script>");	
+			
+			out.close();
+			
+			return null;
+			
+		}else if(update == 0){
+			PrintWriter out = response.getWriter();
+			
+			out.println("<script>");
+			out.println("  alert('비밀번호 오류!');  ");
+			out.println("  history.back(); ");
+			out.println("</script>");	
+			
+			out.close();
+			
+			return null;
+		}
+
+		// 1 수정성공 메시지 출력
+		// 페이지 이동 메인페이지로 이동 (자바스크립트)
+		//check == 1
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<script>");
+		out.println("  alert('정보수정 완료!');  ");
+		out.println("  location.href='./MemberLogin.me'; ");
+		out.println("</script>");	
+		
+		out.close();	
+
 		return null;
 	}
 
