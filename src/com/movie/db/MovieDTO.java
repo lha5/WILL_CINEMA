2e95415e4f08744d2e9b79e1053901e702f9bd55
ITWILL <ITWILL@DESKTOP-BFHD141>
2019-10-18 08:47:17 +0900

    DTO 추가

---
+++ src/com/movie/db/MovieDTO.java
@@ -0,0 +1,91 @@
+package com.movie.db;
+
+public class MovieDTO {
+	int movie_num;
+	String name;
+	String genre;
+	String story;
+	int runing_num;
+	String director;
+	String actor;
+	String open_date;
+	String country;
+	double booking_ration;
+	String poster;
+	String image;
+	
+	public int getMovie_num() {
+		return movie_num;
+	}
+	public void setMovie_num(int movie_num) {
+		this.movie_num = movie_num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public String getStory() {
+		return story;
+	}
+	public void setStory(String story) {
+		this.story = story;
+	}
+	public int getRuning_num() {
+		return runing_num;
+	}
+	public void setRuning_num(int runing_num) {
+		this.runing_num = runing_num;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	public String getActor() {
+		return actor;
+	}
+	public void setActor(String actor) {
+		this.actor = actor;
+	}
+	public String getOpen_date() {
+		return open_date;
+	}
+	public void setOpen_date(String open_date) {
+		this.open_date = open_date;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public double getBooking_ration() {
+		return booking_ration;
+	}
+	public void setBooking_ration(double booking_ration) {
+		this.booking_ration = booking_ration;
+	}
+	public String getPoster() {
+		return poster;
+	}
+	public void setPoster(String poster) {
+		this.poster = poster;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+	
+}
