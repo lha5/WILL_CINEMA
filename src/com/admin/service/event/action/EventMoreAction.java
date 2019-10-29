a4fbbc518eeaa95988a46fbf8a18e49bc909ff80
ITWILL <ITWILL@DESKTOP-BFHD141>
2019-10-29 17:06:17 +0900

    20191029 이승욱

--- src/com/admin/service/event/action/EventMoreAction.java
+++ src/com/admin/service/event/action/EventMoreAction.java
@@ -14,7 +14,7 @@ import com.admin.service.event.db.AdminEventDAO;
 import com.admin.service.event.db.AdminEventDAOImpl;
 import com.admin.service.event.db.AdminEventDTO;
 
-import jdk.nashorn.internal.parser.JSONParser;
+
 
 public class EventMoreAction implements Action {
 
