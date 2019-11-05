e0250f8e39d07f17f322a351c6e68ab41361383e
ITWILL <ITWILL@DESKTOP-BFHD141>
2019-11-05 18:14:49 +0900

    20191105_이승욱 이메일 미완성분

--- src/com/ticketing/action/TestSendEmailAction.java
+++ src/com/ticketing/action/TestSendEmailAction.java
@@ -1,6 +1,6 @@
 package com.ticketing.action;
 
-import java.util.List;
+import java.io.PrintWriter;
 import java.util.Properties;
 
 import javax.mail.Message;
@@ -12,6 +12,7 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.action.Action;
 import com.action.ActionForward;
@@ -20,63 +21,66 @@ import com.member.db.MemberDAOImpl;
 import com.member.db.MemberDTO;
 
 public class TestSendEmailAction implements Action{
- @Override
+	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 	
-	 System.out.println("TestSendEmailAction execute()--------------------------------------------");
+		
+		System.out.println("TestSendEmailAction execute()--------------------------------------------");
 	 
-	 List<MemberDTO> boardList = null;
-	 
-	 MemberDTO mdto = new MemberDTO();
-	 
-	 mdto.setEmail(request.getParameter("email"));
-	 	
-	 	
-	 
-	 
+		MemberDTO mdto = new MemberDTO();
+			
+		
+		MemberDAO mdao = new MemberDAOImpl();
+		
+		
+		String host  = "smtp.naver.com";
+		 // 관리자 메일(보내는 사람) 
+		 final String user   = "alien213";
+		  final String password  = "tmddnr21";
+
+		 
+		//해당 메일로 전송(받는사람) 
+		 
+		
+		 String to =mdao.getMember("test100").getEmail();
+		 System.out.println("--------------------------------");
+		 System.out.println(to);
+		 System.out.println("--------------------------------");
+		
+	  Properties props = new Properties();
+	  props.put("mail.smtp.host", host);
+	  props.put("mail.smtp.auth", "true");
+
+	  Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+	   protected PasswordAuthentication getPasswordAuthentication() {
+	    return new PasswordAuthentication(user, password);
+	   }
+	  });
+
+	  // Compose the message
+	  try {
+	   MimeMessage message = new MimeMessage(session);
+	   message.setFrom(new InternetAddress(user));
+	   message.addRecipient(Message.RecipientType.TO, new InternetAddress());
+
+	   // Subject
+	   message.setSubject("테스트입니다");
+	   
+	   // Text
+	   message.setText("테스트 중입니다");
+
+	   // send the message
+	   Transport.send(message);
+	   System.out.println("message sent successfully...");
+
+	  } catch (MessagingException e) {
+	   e.printStackTrace();
+	  } 
+		
 		return null;
 	}
+	
 
-public static void main(String[] args) {
 
-	 String host  = "smtp.naver.com";
-	  final String user   = "alien213";
-	  final String password  = "tmddnr21";
-
-	  String to  = " ";
-
-  
-	  
-	  
-  // Get the session object
-  Properties props = new Properties();
-  props.put("mail.smtp.host", host);
-  props.put("mail.smtp.auth", "true");
-
-  Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
-   protected PasswordAuthentication getPasswordAuthentication() {
-    return new PasswordAuthentication(user, password);
-   }
-  });
-
-  // Compose the message
-  try {
-   MimeMessage message = new MimeMessage(session);
-   message.setFrom(new InternetAddress(user));
-   message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-
-   // Subject
-   message.setSubject("테스트입니다");
-   
-   // Text
-   message.setText("테스트 중입니다");
-
-   // send the message
-   Transport.send(message);
-   System.out.println("message sent successfully...");
-
-  } catch (MessagingException e) {
-   e.printStackTrace();
-  }
- }
+	 
 }
